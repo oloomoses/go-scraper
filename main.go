0a9bd30778919c5461b494b938270ed2380aff7b
oloomoses <olooine@gmail.com>
2025-12-02 14:06:55 +0300

    Add Product extractor and display extracted products

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	url := "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+	url := "https://books.toscrape.com"
 
 	fmt.Println("Fetching books ....")
 	data, err := crawler.Fetch(url)
@@ -18,6 +18,20 @@ func main() {
 		log.Fatal("error fetching books", err)
 	}
 
-	fmt.Println(parser.ExtractAnkor(data))
+	fmt.Println("Parsing catalogue ...")
+	prods, next, err := parser.ParseCatalogue(data, url)
 
+	if err != nil {
+		log.Fatal("Parse catalogue failed", err)
+	}
+
+	fmt.Println("Products", len(prods))
+
+	for _, p := range prods[:5] {
+		fmt.Printf("%v | %v | %v | %v\n", p.ID, p.Title, p.Price, p.Url)
+	}
+
+	if next != "" {
+		fmt.Println("Next page", next)
+	}
 }
--- parser/parser.go
+++ parser/parser.go
@@ -3,34 +3,108 @@ package parser
 import (
 	"strings"
 
+	"github.com/oloomoses/go-craper/model"
+	"github.com/oloomoses/go-craper/util"
 	"golang.org/x/net/html"
 )
 
-func ExtractAnkor(htmlBody string) []string {
+func ParseCatalogue(htmlBody string, baseUrl string) ([]model.Product, string, error) {
+	var products []model.Product
+
 	doc, err := html.Parse(strings.NewReader(htmlBody))
 
 	if err != nil {
-		return []string{}
+		return nil, "", err
 	}
-	var ankor []string
-	var visitor func(*html.Node)
 
-	visitor = func(node *html.Node) {
-		if node.Type == html.ElementNode && node.Data == "a" {
-			for _, a := range node.Attr {
-				if a.Key == "href" {
-					ankor = append(ankor, a.Val)
+	var nextUrl string
+
+	var walk func(*html.Node)
+
+	walk = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "article" && hasClass(node, "product_pod") {
+			p := extractProduct(node, baseUrl)
+
+			if p != nil {
+				products = append(products, *p)
+			}
+		}
+
+		if node.Type == html.ElementNode && node.Data == "li" && hasClass(node, "next") {
+			if a := findChildByTag(node, "a"); a != nil {
+				for _, a := range node.Attr {
+					if a.Key == "href" {
+						nextUrl = a.Val
+					}
 				}
 			}
-			// return
 		}
 
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
-			visitor(c)
+			walk(c)
 		}
 	}
 
-	visitor(doc)
+	walk(doc)
 
-	return ankor
+	return products, nextUrl, nil
+}
+
+func hasClass(node *html.Node, cls string) bool {
+	for _, a := range node.Attr {
+		if a.Key == "class" && a.Val == cls {
+			return true
+		}
+	}
+	return false
+}
+
+func findChildByTag(n *html.Node, tag string) *html.Node {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == tag {
+			return c
+		}
+	}
+	return nil
+}
+
+func extractProduct(article *html.Node, baseUrl string) *model.Product {
+	titleLink := findChildByTag(article, "h3")
+
+	a := findChildByTag(titleLink, "a")
+
+	title := a.Attr[1].Val
+
+	href := a.Attr[0].Val
+
+	price := getPriceNode(article)
+
+	absUrl, _ := util.Resolve(baseUrl, href)
+
+	return &model.Product{
+		Title: title,
+		Price: price,
+		Url:   absUrl,
+	}
+}
+
+func getPriceNode(n *html.Node) string {
+
+	var price string
+
+	var f func(*html.Node)
+
+	f = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "p" && hasClass(node, "price_color") {
+			price = strings.TrimSpace(node.FirstChild.Data)
+		}
+
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(n)
+
+	return price
 }
